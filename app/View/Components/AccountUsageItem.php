3edfe87084aaa0c94d405aa63c632ebca0137eca
Matthew Setter <matthew@matthewsetter.com>
2023-01-17 18:32:58 +0100

    Simplify the output of the account usage information
    I'm not that familiar with Laravel and all the ins and outs of its
    various parts and components. Given that, I didn't know just how simple
    outputting information via Laravel Blade templates and components could
    be. Now that I've learned more about it, I'm making this commit to
    remove the extra, unnecessary work required to achieve the same result
    as in the previous commit.

--- app/View/Components/AccountUsageItem.php
+++ app/View/Components/AccountUsageItem.php
@@ -3,13 +3,10 @@
 namespace App\View\Components;
 
 use App\Twilio\ArrayRecordInstance;
-use App\Twilio\PrintableRecordInstance;
 use Illuminate\View\Component;
-use Laminas\Hydrator\NamingStrategy\UnderscoreNamingStrategy;
-use Laminas\Hydrator\ReflectionHydrator;
-use Laminas\Hydrator\Strategy\DateTimeFormatterStrategy;
-use Laminas\Hydrator\Strategy\DateTimeImmutableFormatterStrategy;
-use Laminas\Hydrator\Strategy\ScalarTypeStrategy;
+use Money\Currencies\ISOCurrencies;
+use Money\Formatter\IntlMoneyFormatter;
+use NumberFormatter;
 use Twilio\Rest\Api\V2010\Account\Usage\RecordInstance;
 
 class AccountUsageItem extends Component
@@ -18,45 +15,37 @@ class AccountUsageItem extends Component
      * @var array<int,RecordInstance>
      */
     public array $usageRecords;
+    private IntlMoneyFormatter $moneyFormatter;
+    private NumberFormatter $formatter;
 
     /**
      * @param array<int,RecordInstance> $usageRecords
      */
     public function __construct(array $usageRecords)
     {
-        $hydrator = new ReflectionHydrator();
-        $hydrator->setNamingStrategy(new UnderscoreNamingStrategy());
-        $hydrator->addStrategy(
-            'asOf',
-            new DateTimeImmutableFormatterStrategy(
-                new DateTimeFormatterStrategy('Y-m-d\TG:i:sP')
-            )
-        );
-        $hydrator->addStrategy(
-            'endDate',
-            new DateTimeImmutableFormatterStrategy(
-                new DateTimeFormatterStrategy('Y-m-d\TG:i:sP')
-            )
-        );
-        $hydrator->addStrategy(
-            'startDate',
-            new DateTimeImmutableFormatterStrategy(
-                new DateTimeFormatterStrategy('Y-m-d\TG:i:sP')
-            )
-        );
-        $hydrator->addStrategy('count', ScalarTypeStrategy::createToInt());
-        $hydrator->addStrategy('price', ScalarTypeStrategy::createToInt());
-        $hydrator->addStrategy('usage', ScalarTypeStrategy::createToInt());
+        $this->usageRecords = $usageRecords;
 
-        $subresourceUriHydrator = new ReflectionHydrator();
-        $subresourceUriHydrator->setNamingStrategy(new UnderscoreNamingStrategy());
+        $currencies = new ISOCurrencies();
+        $this->formatter = new NumberFormatter(
+            'en_US', NumberFormatter::CURRENCY
+        );
+        $this->moneyFormatter = new IntlMoneyFormatter(
+            $this->formatter, $currencies
+        );
+    }
 
-        foreach ($usageRecords as $usageRecord) {
-            $this->usageRecords[] = $hydrator->hydrate(
-                $usageRecord->toArray(),
-                new PrintableRecordInstance()
-            );
-        }
+    public function getFormattedPrice(string $price, string $priceUnit): string
+    {
+        return $this->formatter->formatCurrency(
+            (float)$price,
+            strtoupper($priceUnit)
+        );
+    }
+
+    public function formatDateAsString(string $date, string $shortOrLong = 'short'): string
+    {
+        return (new \DateTimeImmutable($date))
+            ->format($shortOrLong === 'long' ? 'd-m-Y G:i' : 'd-m-Y');
     }
 
     /**
--- resources/views/components/account-usage-item.blade.php
+++ resources/views/components/account-usage-item.blade.php
@@ -1,11 +1,11 @@
 @foreach ($usageRecords as $usageRecord)
     <tr>
         <td>
-            <span class="hidden sm:block">{{ $usageRecord->getAsOf()->format('d-m-Y G:i') }}</span>
-            <span class="block sm:hidden">{{ $usageRecord->getAsOf()->format('d-m-Y') }}</span>
+            <span class="hidden sm:block">{{ $formatDateAsString($usageRecord->asOf, 'long') }}</span>
+            <span class="block sm:hidden">{{ $formatDateAsString($usageRecord->asOf) }}</span>
         </td>
-        <td>{{ $usageRecord->getDescription() }}</td>
-        <td class="hidden sm:block">{{ $usageRecord->getCount() }}</td>
-        <td>{{ $usageRecord->getFormattedPrice() }}</td>
+        <td>{{ $usageRecord->description }}</td>
+        <td class="hidden sm:block">{{ $usageRecord->count }}</td>
+        <td>{{ $getFormattedPrice($usageRecord->price, $usageRecord->priceUnit) }}</td>
     </tr>
 @endforeach
